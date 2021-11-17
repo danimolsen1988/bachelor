562e4ff28a6996a67f16846835191bbfdff814e5
danimolsen1988 <201808188@post.au.dk>
2021-11-17 16:01:11 +0100

    Added MVC web and admin lte

---
+++ ManagementWebAppMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ManagementWebAppMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
